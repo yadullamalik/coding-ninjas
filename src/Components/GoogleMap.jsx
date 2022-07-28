0f1d689fd889bbeb495c8319cdc359b3b3622770
yadullamalik <shaikyadullamalik123@gmail.com>
2022-07-28 20:03:06 +0530

    update

--- src/Components/GoogleMap.jsx
+++ src/Components/GoogleMap.jsx
@@ -1,159 +1,92 @@
 import { useEffect, useState, useRef } from "react";
-import {
-  Box,
-  Button,
-  ButtonGroup,
-  Flex,
-  HStack,
-  IconButton,
-  Input,
-  SkeletonText,
-  Text,
-} from "@chakra-ui/react";
 import { FaLocationArrow, FaTimes } from "react-icons/fa";
 import {
   useJsApiLoader,
   GoogleMap,
   Marker,
-  Autocomplete,
   DirectionsRenderer,
 } from "@react-google-maps/api";
+const google = window.google;
 
-export const Google_Map = () => {
-  const [lat, setLat] = useState("");
-  const [lon, setLon] = useState("");
-  const center = { lat: lat, lng: lon };
-  useEffect(() => {
-    navigator.geolocation.getCurrentPosition((position) => {
-      setLat(position.coords.latitude);
-      setLon(position.coords.longitude);
+export const Google_Map = ({ lat, lon, city }) => {
+  var map;
+  var infowindow;
+
+  function initialize() {
+    var pyrmont = new google.maps.LatLng(19.107567, 72.8335); // sample location to start with: Mumbai, India
+
+    map = new google.maps.Map(document.getElementById("map-canvas"), {
+      center: pyrmont,
+      zoom: 15,
     });
-  }, [lat]);
-  const { isLoaded } = useJsApiLoader({
-    googleMapsApiKey: import.meta.env.REACT_APP_GOOGLE_MAPS_API_KEY,
-    libraries: ["places"],
-  });
 
-  const [map, setMap] = useState(/** @type google.maps.Map */ (null));
-  const [directionsResponse, setDirectionsResponse] = useState(null);
-  const [distance, setDistance] = useState("");
-  const [duration, setDuration] = useState("");
-
-  /** @type React.MutableRefObject<HTMLInputElement> */
-  const originRef = useRef();
-  /** @type React.MutableRefObject<HTMLInputElement> */
-  const destiantionRef = useRef();
-
-  if (!isLoaded) {
-    return <div>Loading...</div>;
+    var request = {
+      location: pyrmont,
+      radius: 200,
+      types: ["hospital", "health"], // this is where you set the map to get the hospitals and health related places
+    };
+    infowindow = new google.maps.InfoWindow();
+    var service = new google.maps.places.PlacesService(map);
+    service.nearbySearch(request, callback);
   }
 
-  async function calculateRoute() {
-    if (originRef.current.value === "" || destiantionRef.current.value === "") {
-      return;
+  function callback(results, status) {
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
+      for (var i = 0; i < results.length; i++) {
+        createMarker(results[i]);
+      }
     }
-    // eslint-disable-next-line no-undef
-    const directionsService = new google.maps.DirectionsService();
-    const results = await directionsService.route({
-      origin: originRef.current.value,
-      destination: destiantionRef.current.value,
-      // eslint-disable-next-line no-undef
-      travelMode: google.maps.TravelMode.DRIVING,
+  }
+
+  function createMarker(place) {
+    var placeLoc = place.geometry.location;
+    var marker = new google.maps.Marker({
+      map: map,
+      position: place.geometry.location,
     });
-    setDirectionsResponse(results);
-    setDistance(results.routes[0].legs[0].distance.text);
-    setDuration(results.routes[0].legs[0].duration.text);
+
+    google.maps.event.addListener(marker, "click", function () {
+      infowindow.setContent(place.name);
+      infowindow.open(map, this);
+    });
   }
 
-  function clearRoute() {
-    setDirectionsResponse(null);
-    setDistance("");
-    setDuration("");
-    originRef.current.value = "";
-    destiantionRef.current.value = "";
-  }
+  google.maps.event.addEventListener(window, "load", initialize);
 
+  //   const center = { lat: lat, lng: lon };
+  //   const { isLoaded } = useJsApiLoader({
+  //     googleMapsApiKey: import.meta.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+  //     libraries: ["places"],
+  //   });
+
+  //   const [map, setMap] = useState(/** @type google.maps.Map */ (null));
+  //   //   const [directionsResponse, setDirectionsResponse] = useState(null);
+  //   const [distance, setDistance] = useState("");
+  //   const [duration, setDuration] = useState("");
+  //   useEffect(() => {
+  //     getData();
+  //   }, []);
+
+  //   const getData = async () => {
+  //     const directionsService = new google.maps.DirectionsService();
+  //     const results = await directionsService.route({
+  //       origin: city,
+  //       destination: "hospital",
+  //       travelMode: google.maps.TravelMode.DRIVING,
+  //     });
+  //     // setDirectionsResponse(results);
+  //     console.log("results:", results);
+  //     setDistance(results.routes[0].legs[0].distance.text);
+  //     setDuration(results.routes[0].legs[0].duration.text);
+  //   };
+
+  //   console.log("hi", distance, duration);
+  //   if (!isLoaded) {
+  //     return <div>Loading...</div>;
+  //   }
   return (
     <>
-      {/* <Flex
-        position="relative"
-        flexDirection="column"
-        alignItems="center"
-        h="500px"
-        w="1000px"
-      >
-        <Box position="absolute" left={0} top={0} h="100%" w="100%">
-          
-          <GoogleMap
-            center={center}
-            zoom={15}
-            mapContainerStyle={{ width: "100%", height: "100%" }}
-            options={{
-              zoomControl: false,
-              streetViewControl: false,
-              mapTypeControl: false,
-              fullscreenControl: false,
-            }}
-            onLoad={(map) => setMap(map)}
-          >
-            <Marker position={center} />
-            {directionsResponse && (
-              <DirectionsRenderer directions={directionsResponse} />
-            )}
-          </GoogleMap>
-        </Box>
-        <Box
-          p={4}
-          borderRadius="lg"
-          m={4}
-          bgColor="white"
-          shadow="base"
-          minW="container.md"
-          zIndex="1"
-        >
-          <HStack spacing={2} justifyContent="space-between">
-            <Box flexGrow={1}>
-              <Autocomplete>
-                <Input type="text" placeholder="Origin" ref={originRef} />
-              </Autocomplete>
-            </Box>
-            <Box flexGrow={1}>
-              <Autocomplete>
-                <Input
-                  type="text"
-                  placeholder="Destination"
-                  ref={destiantionRef}
-                />
-              </Autocomplete>
-            </Box>
-
-            <ButtonGroup>
-              <Button colorScheme="pink" type="submit" onClick={calculateRoute}>
-                Calculate Route
-              </Button>
-              <IconButton
-                aria-label="center back"
-                icon={<FaTimes />}
-                onClick={clearRoute}
-              />
-            </ButtonGroup>
-          </HStack>
-          <HStack spacing={4} mt={4} justifyContent="space-between">
-            <Text>Distance: {distance} </Text>
-            <Text>Duration: {duration} </Text>
-            <IconButton
-              aria-label="center back"
-              icon={<FaLocationArrow />}
-              isRound
-              onClick={() => {
-                map.panTo(center);
-                map.setZoom(15);
-              }}
-            />
-          </HStack>
-        </Box>
-      </Flex> */}
-      <div
+      {/* <div
         style={{
           display: "flex",
           width: "500px",
@@ -182,23 +115,19 @@ export const Google_Map = () => {
             onLoad={(map) => setMap(map)}
           >
             <Marker position={center} />
-            {directionsResponse && (
+            {/* {directionsResponse && (
               <DirectionsRenderer directions={directionsResponse} />
-            )}
+            )} 
           </GoogleMap>
         </div>
-        <div style={{ backgroundColor: "white", zIndex: "1" }}>
-          <form>
-            <Autocomplete>
-              <input type="text" ref={originRef} />
-            </Autocomplete>
-            <Autocomplete>
-              <input type="text" ref={destiantionRef} />
-            </Autocomplete>
-            <button type="submit" onClick={calculateRoute}>
-              Submit
-            </button>
-          </form>
+        <div
+          style={{
+            backgroundColor: "white",
+            zIndex: "1",
+            padding: "4px",
+            margin: "4px",
+          }}
+        >
           <div>
             {distance}-{duration}
             <br />
@@ -212,7 +141,10 @@ export const Google_Map = () => {
             </button>
           </div>
         </div>
-      </div>
+      </div> */}
+      <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=places"></script>
+
+      <div id="map-canvas" style={{ height: "500px", width: "500px" }}></div>
     </>
   );
 };
